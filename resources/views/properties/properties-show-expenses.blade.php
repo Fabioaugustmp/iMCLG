506f349814178207e522190643edf26f7c809712
Fabioaugustmp <fabioaugustmp16@gmail.com>
2021-05-14 01:24:25 -0300

    correção aitvo

--- resources/views/properties/properties-show-expenses.blade.php
+++ resources/views/properties/properties-show-expenses.blade.php
@@ -61,7 +61,7 @@
                                                 <td>{{$expense->paymentdate}}</td>
                                                 <td>{{$expense->competence}}</td>
                                                 <td>R$ {{$expense->value}}</td>
-                                                <td align="center"><a href="{{route('expense.show.unique', $properties->id)}}"><i class="far fa-eye"></i></a> </td>
+                                                <td align="center"><a href="{{route('expense.show.unique', $expense->id)}}"><i class="far fa-eye"></i></a> </td>
                                                 <td align="center"><a href="{{ route('expense.edit', $expense->id) }}"><i class="far fa-edit"></i></a> </td>
                                             </tr>
                                             @endforeach
